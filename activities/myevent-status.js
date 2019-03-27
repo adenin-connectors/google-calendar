79afe5146ed95dbea5a9d3bb37e22414b64ffd52
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-27 09:41:55 +0100

    Fixed issue with time calculations.

--- activities/myevent-status.js
+++ activities/myevent-status.js
@@ -13,7 +13,7 @@ module.exports = async (activity) => {
     let eventStatus = {
       title: T('Events Today'),
       url: 'https://calendar.google.com/calendar',
-      urlLabel: T('All events'),
+      urlLabel: T('All events')
     };
 
     let eventCount = events.length;
@@ -67,43 +67,35 @@ function getNexEvent(events) {
 
   return nextEvent;
 }
-/** calculaes time diference between now(utc0) and next event */
-function calculateTimeDiference(nextEventsTime) {
-  let d = new Date(); //nowInMilis
-  var nowUTC = Date.UTC(d.getFullYear(), d.getMonth(), d.getUTCDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds());
-  let nextEventMilis = Date.parse(nextEventsTime);
 
-  return new Date(nextEventMilis - nowUTC);
-}
 //** checks if event is in less then hour, today or tomorrow and returns formated string accordingly */
 function getEventFormatedTimeAsString(nextEvent) {
-  let timeUntilEvent = calculateTimeDiference(nextEvent.start.dateTime);
-  let eventDate = new Date();
+  let eventTime = moment(nextEvent.start.dateTime)
+    .tz(Activity.Context.UserTimezone)
+    .locale(Activity.Context.UserLocale);
+  let timeNow = moment(new Date());
 
-  if (timeUntilEvent.getHours() == 0) {
+  let diffInHrs = eventTime.diff(timeNow, 'hours');
+
+  if (diffInHrs == 0) {
     //events that start in less then 1 hour
-    return T(` in {0} minutes.`, timeUntilEvent.getMinutes());
+    let diffInMins = eventTime.diff(timeNow, 'minutes');
+    return T(` in {0} minutes.`, diffInMins);
   } else {
     //events that start in more than 1 hour
-    let eventClock = moment(eventDate)
-      .tz(Activity.Context.UserTimezone)
-      .locale(Activity.Context.UserLocale)
-      .format('LT');
+    let diffInDays = eventTime.diff(timeNow, 'days');
 
     let datePrefix = '';
     let momentDate = '';
-    if (timeUntilEvent.getDate() == 2) {
+    if (diffInDays == 1) {
       //events that start tomorrow
       datePrefix = ' tomorrow';
-    } else if (timeUntilEvent.getDate() > 2) {
+    } else if (diffInDays > 1) {
       //events that start day after tomorrow and later
       datePrefix = ' on ';
-      momentDate = moment(eventDate)
-        .tz(Activity.Context.UserTimezone)
-        .locale(Activity.Context.UserLocale)
-        .format('LL');
+      momentDate = eventTime.format('LL');
     }
 
-    return T(`{0}{1}{2}{3}.`, T(datePrefix), momentDate, T(" at "), eventClock);
+    return T(`{0}{1}{2}{3}.`, T(datePrefix), momentDate, T(" at "), eventTime.format('LT'));
   }
 }
